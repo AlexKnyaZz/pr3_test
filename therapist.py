16603056d10ea157ecdb38a459ae560accf96fb7
AlexKnyaZz <alexknyazz@yandex.ru>
2022-12-15 09:48:45 +0300

    def_questions Test

--- therapist.py
+++ therapist.py
@@ -2,3 +2,20 @@ headache = None
 high_temperature = None
 muscle_pain = None
 cough = None
+
+def get_questions_test():
+    global headache
+    print("Вы чувствуете головную боль? (1 - да, 2 - нет)")
+    answer = int(input())
+    if answer == 1:
+        headache = True
+    else:
+        headache = False
+
+
+def get_questions():
+    pass
+
+
+get_questions_test()
+print(headache)
